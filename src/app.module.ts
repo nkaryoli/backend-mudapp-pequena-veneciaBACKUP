de335ebefd8073d5ddeba613ca2cdd9d436537b8
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-02-04 20:49:12 +0100

    primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientModule } from './client/client.module';
+import { CarrierModule } from './carrier/carrier.module';
+import { ServiceModule } from './service/service.module';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { AuthModule } from './auth/auth.module';
+import config from './config/config';
+import { PaymentModule } from './payment/payment.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+    }),
+    UserModule,
+    ClientModule,
+    CarrierModule,
+    ServiceModule,
+    VehicleModule,
+    PaymentModule, //Faltaba el modulo payment
+    AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
